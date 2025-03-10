8e36d49f484b63de5e2737d6994675b163dab224
codeflow <codeflow110101@gmail.com>
2025-03-10 20:34:56 +0530

    testing

--- app/Http/Middleware/VaildatePayment.php
+++ app/Http/Middleware/VaildatePayment.php
@@ -16,11 +16,14 @@ class VaildatePayment
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->has('com_squareup_pos_CLIENT_TRANSACTION_ID') && Payment::where('transaction_id', $request->query('com_squareup_pos_CLIENT_TRANSACTION_ID'))->doesntExist()) {
+        if ($request->has('com_squareup_pos_CLIENT_TRANSACTION_ID') && Payment::where('transaction_id', $request->query('com_squareup_pos_CLIENT_TRANSACTION_ID'))->doesntExist() && session()->has('cart') && session()->has('checkout_user_id') && session()->has('checkout_coupon_id')) {
+            dd(session('cart'), session('checkout_user_id'), session('checkout_coupon_id'));
         }
         dd(Payment::where('transaction_id', $request->query('com_squareup_pos_CLIENT_TRANSACTION_ID'))->doesntExist());
 
         dd($request->query('com_squareup_pos_CLIENT_TRANSACTION_ID'));
+        // session()->forget('cart');
+
         return $next($request);
     }
 }
