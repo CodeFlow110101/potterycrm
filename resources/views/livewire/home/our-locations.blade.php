870443d89e891d990a2874a95f0f36ea38a1b20b
Nishant Kedare <kedarenishant11111@gmail.com>
2025-11-03 21:16:05 +0530

    Our loaction name changed

--- resources/views/livewire/home/our-locations.blade.php
+++ resources/views/livewire/home/our-locations.blade.php
@@ -5,7 +5,7 @@ use function Livewire\Volt\{state};
 ?>
 
 <div class="w-11/12 mx-auto gap-4 lg:gap-8 py-4 lg:py-8 text-white grow flex flex-col">
-    <div class="text-5xl lg:text-7xl font-avenir-next-bold">Our Locations</div>
+    <div class="text-5xl lg:text-7xl font-avenir-next-bold">Our Location</div>
     <div class="grow relative" x-data="{ height: 0 }" x-resize="height = $height">
         <div class="backdrop-blur-xl border border-white rounded-lg overflow-y-auto hidden-scrollbar absolute inset-x-0" :style="'height: ' + height + 'px;'">
             <div class="flex flex-col gap-2 p-4">
